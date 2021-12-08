06337e65f12450f10d67ed1dbf25e0f72a2c3f6a
Fabulosa <fabiscg79@gmail.com>
2021-12-08 12:06:10 -0500

    added some questions about the user and the project

--- app.js
+++ app.js
@@ -1,19 +1,93 @@
+// const fs = require('fs');
+// const generatePage = require('./scr/page-template');
 
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
-const generatePage = require('./src/page-template.js');
-
+const inquirer = require('inquirer')
 
 
 
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+
+};
 
 
-fs.writeFile('./index.html', generatePage([profileDataArgs]), err => {
-  if (err) throw new Error(err);
 
-  console.log("Portfolio completed! Check out index.html to see the output!");
-})
+const promptProject = () => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)'
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)'
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ]);
+};
+
+
+promptUser()
+  .then(answers => console.log(answers))
+  .then(promptProject)
+  .then(projectAnswers => console.log(projectAnswers));
+
+
+
+  
+
+
+
+// fs.writeFile('./index.html', pageHTML, err => {
+
+
+//   console.log("Portfolio completed! Check out index.html to see the output!");
+// })
 
 
 
