0669547b262c9663cd8897a0c0e6765962ac0b41
Fabulosa <fabiscg79@gmail.com>
2021-12-13 11:02:57 -0500

    refactored the code

--- app.js
+++ app.js
@@ -134,18 +134,47 @@ const promptProject = portfolioData => {
   });
 };
 
-
 promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
+.then(promptProject)
+.then(portfolioData => {
+  return generatePage(portfolioData);
+})
+.then(pageHTML => {
+  return writeFile(pageHTML)
+})
+.then(writeFileResponse => {
+  console.log(writeFileResponse)
+  return copyFile();
+})
+.then(copyFileResponse => {
+  console.log(copyFileResponse);
+})
+.catch(err => {
+  console.log(err);
+})
 
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
-  });
+
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     console.log(portfolioData);
+//     const pageHTML = generatePage(portfolioData);
+    // fs.writeFile('./dist/index.html', pageHTML, err => {
+    //   if (err) {
+    //     console.log(err);
+    //     return;
+    //   }
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    
+    //   fs.copyFile('./src/style.css', './dist/style.css', err => {
+    //     if (err) {
+    //       console.log(err);
+    //       return;
+    //     }
+    //     console.log('Style sheet copied successfully!');
+    //   });
+    // });
+  // })
 
 
 
